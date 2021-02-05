0e1871a9e6ed108805bfc8000c57ba888eba2554
Joan C. Ayala <jflaco85@gmail.com>
2021-02-05 12:17:39 -0500

    Add Folder/File Data Base connection module

---
+++ database/config.js
@@ -0,0 +1,13 @@
+const Pool = require('pg').Pool;
+
+const dbConnection = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+module.exports = {
+  dbConnection,
+};
