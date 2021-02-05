01427e1f74c697ef3fad9908d56af05ee8c84b52
Joan C. Ayala <jflaco85@gmail.com>
2021-02-05 12:13:11 -0500

    Add index.js file (Express server and app services configurations)

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+// Create server express
+const app = express();
+
+// Setting CORS
+app.use(cors());
+
+// Read and parsing body
+app.use(express.json());
+
+// Endpoints
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    status: true,
+    msg: 'Welcome vertebra-api',
+  });
+});
+app.use('/api/login', require('./src/auth/auth.router'));
+app.use('/api/users', require('./src/user/user.route'));
+
+// Main
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: ${process.env.PORT}`);
+});
